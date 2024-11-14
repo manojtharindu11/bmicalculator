0db1cdda4ea628092454a4923f405da1bf9e689b
Tharindu Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2024-11-14 16:22:32 +0530

    create simple style scheme for text

---
+++ lib/constant.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const kTextColor = Color(0xffffb047);
+const kOutputTextColor = Color(0xFFFF9047);
+
+const kInputLabelColor =
+    TextStyle(color: kTextColor, fontSize: 50, fontWeight: FontWeight.bold);
